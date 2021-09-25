dacb7ae6c906c68d49eb052fd9cec7b84d09de3a
Viktor Zalkin <zalkin.viktor@mail.ru>
2021-09-25 15:49:27 +0300

    Add changing background color of textView by switching

--- app/src/main/java/com/itmo/basiclayout/MainActivity.kt
+++ app/src/main/java/com/itmo/basiclayout/MainActivity.kt
@@ -2,10 +2,23 @@ package com.itmo.basiclayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    val switcher: Switch by lazy { findViewById(R.id.switch1) }
+    val textView: TextView by lazy { findViewById(R.id.textView) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        switcher.setOnCheckedChangeListener { _, isChecked ->
+            val colorResource =
+                if (isChecked) R.color.label_color_switch_true
+                else R.color.label_color_switch_false
+
+            textView.setBackgroundResource(colorResource)
+        }
     }
 }
