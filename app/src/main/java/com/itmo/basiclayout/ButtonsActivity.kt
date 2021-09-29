eb363287c52e45ddbc4161b081149bdc014fc1b9
Viktor Zalkin <zalkin.viktor@mail.ru>
2021-09-29 19:52:50 +0300

    Set click listeners on two buttons to show toast and log

--- app/src/main/java/com/itmo/basiclayout/ButtonsActivity.kt
+++ app/src/main/java/com/itmo/basiclayout/ButtonsActivity.kt
@@ -2,10 +2,30 @@ package com.itmo.basiclayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class ButtonsActivity : AppCompatActivity() {
+
+    val buttonTask1: Button by lazy { findViewById(R.id.button_task1) }
+    val buttonTask2: Button by lazy { findViewById(R.id.button_task2) }
+    val buttonTask3: Button by lazy { findViewById(R.id.button_task3) }
+
+    companion object {
+        private const val logInfoTag = "INFO_BUTTONS"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buttons)
+
+        buttonTask1.setOnClickListener {
+            Toast.makeText(this, "Not implemented, yet!", Toast.LENGTH_SHORT).show()
+        }
+
+        buttonTask2.setOnClickListener {
+            Log.d(logInfoTag, "Button 2: Not implemented, yet!")
+        }
     }
 }
