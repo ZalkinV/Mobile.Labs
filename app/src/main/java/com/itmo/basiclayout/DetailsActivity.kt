6dab02f374df21bbee80215048eaf29d9567b36a
Viktor Zalkin <zalkin.viktor@mail.ru>
2021-10-10 19:39:50 +0300

    Task1.Details. Add listeners for button with fake sequences generation

--- app/src/main/java/com/itmo/basiclayout/DetailsActivity.kt
+++ app/src/main/java/com/itmo/basiclayout/DetailsActivity.kt
@@ -2,6 +2,7 @@ package com.itmo.basiclayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -11,24 +12,47 @@ class DetailsActivity : AppCompatActivity() {
     private lateinit var description: TextView
     private lateinit var imageViewIcon: ImageView
 
+    private lateinit var buttonNat: Button
+    private lateinit var textViewNat: TextView
+
+    private lateinit var buttonFib: Button
+    private lateinit var textViewFib: TextView
+
+    private lateinit var buttonCol: Button
+    private lateinit var textViewCol: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
+        initializeComponents()
+
         val listItemDetails = intent.getSerializableExtra(IntentKeys.Task1.DETAILS.name)
         if (listItemDetails is ListItemDetails) {
-            initializeComponents(listItemDetails)
+            applyListItemDetails(listItemDetails)
         }
+
+        setListeners()
     }
 
-    private fun initializeComponents(details: ListItemDetails) {
+    private fun initializeComponents() {
         imageLabel = findViewById(R.id.imageLabel)
-        imageLabel.text = details.title
-
         description = findViewById(R.id.description)
-        description.text = details.description
-
         imageViewIcon = findViewById(R.id.imageViewIcon)
+
+        buttonNat = findViewById(R.id.buttonNat)
+        textViewNat = findViewById(R.id.textViewNat)
+
+        buttonFib = findViewById(R.id.buttonFib)
+        textViewFib = findViewById(R.id.textViewFib)
+
+        buttonCol = findViewById(R.id.buttonCol)
+        textViewCol = findViewById(R.id.textViewCol)
+    }
+
+    private fun applyListItemDetails(details: ListItemDetails) {
+        imageLabel.text = details.title
+        description.text = details.description
         val iconId = when(details.icon) {
             IconEnum.SCANNER -> R.drawable.ic_outline_adf_scanner_24
             IconEnum.CLOUD -> R.drawable.ic_outline_cloud_24
@@ -36,5 +60,25 @@ class DetailsActivity : AppCompatActivity() {
             IconEnum.DESCRIPTION -> R.drawable.ic_outline_description_24
         }
         imageViewIcon.setImageResource(iconId)
+    }
+
+    private fun setListeners() {
+        var sequenceNat = 1;
+        buttonNat.setOnClickListener {
+            sequenceNat += 1
+            textViewNat.text = sequenceNat.toString()
+        }
+
+        var sequenceFib = 2;
+        buttonFib.setOnClickListener {
+            sequenceFib += 2
+            textViewFib.text = sequenceFib.toString()
+        }
+
+        var sequenceCol = 1;
+        buttonCol.setOnClickListener {
+            sequenceCol += 3
+            textViewCol.text = sequenceCol.toString()
+        }
     }
 }
