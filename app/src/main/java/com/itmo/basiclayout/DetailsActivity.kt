ef6685cf75ea201fe924be91d77effe4a02b1bb1
Viktor Zalkin <zalkin.viktor@mail.ru>
2021-10-10 17:54:27 +0300

    Task1.Details. Add initializeComponents() with info from intent

--- app/src/main/java/com/itmo/basiclayout/DetailsActivity.kt
+++ app/src/main/java/com/itmo/basiclayout/DetailsActivity.kt
@@ -1,14 +1,40 @@
 package com.itmo.basiclayout
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class DetailsActivity : AppCompatActivity() {
+
+    private lateinit var imageLabel: TextView
+    private lateinit var description: TextView
+    private lateinit var imageViewIcon: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
-        val listItemDetails = intent.getSerializableExtra(IntentKeys.Task1.DETAILS.name) as ListItemDetails?
+        val listItemDetails = intent.getSerializableExtra(IntentKeys.Task1.DETAILS.name)
+        if (listItemDetails is ListItemDetails) {
+            initializeComponents(listItemDetails)
+        }
+    }
+
+    private fun initializeComponents(details: ListItemDetails) {
+        imageLabel = findViewById(R.id.imageLabel)
+        imageLabel.text = details.title
+
+        description = findViewById(R.id.description)
+        description.text = details.description
+
+        imageViewIcon = findViewById(R.id.imageViewIcon)
+        val iconId = when(details.icon) {
+            IconEnum.SCANNER -> R.drawable.ic_outline_adf_scanner_24
+            IconEnum.CLOUD -> R.drawable.ic_outline_cloud_24
+            IconEnum.CORONAVIRUS -> R.drawable.ic_outline_coronavirus_24
+            IconEnum.DESCRIPTION -> R.drawable.ic_outline_description_24
+        }
+        imageViewIcon.setImageResource(iconId)
     }
 }
