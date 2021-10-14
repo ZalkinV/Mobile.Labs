7a9f2c030a7b3d64a4db2da6066426bc390524de
Viktor Zalkin <zalkin.viktor@mail.ru>
2021-10-14 21:57:05 +0300

    Task1.DataSourse.fetchData(). Make list initialization with lambda

--- app/src/main/java/com/itmo/basiclayout/DataSource.kt
+++ app/src/main/java/com/itmo/basiclayout/DataSource.kt
@@ -3,18 +3,15 @@ package com.itmo.basiclayout
 import kotlin.random.Random
 
 class DataSource {
-    val names = listOf("Ann", "Bob", "Alice", "Maria", "Marting", "Carl")
-
-    fun fetchData(elementsCount: Int): MutableList<ListItemDetails> {
-        val listItems = mutableListOf<ListItemDetails>()
-
-        for (i in 1..elementsCount) {
+    fun fetchData(elementsCount: Int): List<ListItemDetails> {
+        val listItems = List(elementsCount) {
             val iconIndex = Random.nextInt(0, IconEnum.values().size)
             val icon = IconEnum.values()[iconIndex]
-            listItems.add(ListItemDetails(
-                "Title $i",
-                "Descritpion $i",
-                icon))
+
+            ListItemDetails(
+                "Title $it",
+                "Description $it",
+                icon)
         }
 
         return listItems
