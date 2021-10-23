bff8de47eb2c032a4254b163291f450856c9b1f9
Viktor Zalkin <zalkin.viktor@mail.ru>
2021-10-23 15:43:08 +0300

    Extra: Buttons. Add decreasing and increasing course points on clicks

--- app/src/main/java/com/itmo/basiclayout/ButtonsActivity.kt
+++ app/src/main/java/com/itmo/basiclayout/ButtonsActivity.kt
@@ -7,9 +7,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.content.edit
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.widget.doOnTextChanged
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
@@ -18,6 +22,10 @@ class ButtonsActivity : AppCompatActivity() {
     companion object {
         private const val logTag = "BUTTONS"
     }
+
+    private lateinit var button_decrease: ImageButton
+    private lateinit var button_increase: ImageButton
+    private lateinit var textView_points: TextView
 
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navigationView: NavigationView
@@ -31,6 +39,11 @@ class ButtonsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_buttons)
 
         initializePreferences()
+
+        findComponents()
+        setListeners()
+        initializeComponents()
+
         initializeDrawer()
     }
 
@@ -45,6 +58,41 @@ class ButtonsActivity : AppCompatActivity() {
     private fun initializePreferences() {
         preferences = getSharedPreferences(PreferencesEnum.Buttons.PREF_NAME.name, Context.MODE_PRIVATE)
         coursePoints = preferences.getInt(PreferencesEnum.Buttons.COURSE_POINTS.name, 0)
+    }
+
+    private fun findComponents() {
+        button_decrease = findViewById(R.id.button_coursePoints_decrease)
+        button_increase = findViewById(R.id.button_coursePoints_increase)
+        textView_points = findViewById(R.id.textView_coursePoints)
+    }
+
+    private fun initializeComponents() {
+        textView_points.text = coursePoints.toString()
+    }
+
+    private fun setListeners() {
+        button_decrease.setOnClickListener {
+            if (coursePoints > 0)
+                textView_points.text = (--coursePoints).toString()
+        }
+
+        button_increase.setOnClickListener {
+            if (coursePoints < 999)
+                textView_points.text = (++coursePoints).toString()
+        }
+
+        textView_points.doOnTextChanged { _, _, _, _ ->
+            val color = when {
+                coursePoints < 50 -> R.color.red
+                coursePoints < 60 -> R.color.orange
+                coursePoints < 80 -> R.color.yellow
+                else -> R.color.green
+            }
+
+            val newTextViewColor = ResourcesCompat.getColor(resources, color, null)
+            if (newTextViewColor != textView_points.currentTextColor)
+                textView_points.setTextColor(newTextViewColor)
+        }
     }
 
     /* How to make drawer:
