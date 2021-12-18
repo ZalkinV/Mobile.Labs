a71372355268bbac11a2a0ba2ab503db61a1df60
Viktor Zalkin <zalkin.viktor@mail.ru>
2021-12-18 12:48:18 +0300

    T2. Create Task2 empty activity

---
+++ app/src/main/java/com/itmo/basiclayout/task2/Task2Activity.kt
@@ -0,0 +1,12 @@
+package com.itmo.basiclayout.task2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.itmo.basiclayout.R
+
+class Task2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task2)
+    }
+}
